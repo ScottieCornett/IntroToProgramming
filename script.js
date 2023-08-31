eba28e53cd46481806ea81cc75dadfcb7791f712
ScottieCornett <scottie.cornett@gmail.com>
2023-08-31 16:35:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('hello world!');
+
+const input = Number(prompt('What is your favorite number?'));
+
+if (input === 25) {
+    console.log(`That's my favorite too!`)
+}else {
+    console.log(`Also a great number!`);
+}
